41e8daf2a81e91254677a5beffc8f577a98790b4
bharath0292 <bharath0292@gmail.com>
2022-07-25 11:09:18 +0530

    Create method added

--- Controllers/MakeController.cs
+++ Controllers/MakeController.cs
@@ -20,5 +20,22 @@ namespace DreyBikeShop.Controllers
         {
             return View(_db.Makes.ToList());
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Make make)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(make);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(make);
+        }
     }
 }
