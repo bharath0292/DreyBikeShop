db9a38315d24c20d3aaba3ecc65c9fa4f459afc5
bharath0292 <bharath0292@gmail.com>
2022-07-25 15:22:20 +0530

    Edit operation added

--- Controllers/MakeController.cs
+++ Controllers/MakeController.cs
@@ -51,5 +51,26 @@ namespace DreyBikeShop.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Edit(int id)
+        {
+            var make = _db.Makes.Find(id);
+            if (make == null)
+            {
+                return NotFound();
+            }
+            return View(make);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Make make)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(make);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(make);
+        }
     }
 }
